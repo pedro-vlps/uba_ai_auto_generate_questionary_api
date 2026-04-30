cfac601044b521484c79c0636dad9975b180e964
Pedro Vieira <pedro515vieira@gmail.com>
2026-04-30 16:19:39 -0300

    New Front end deploy configs

--- src/configs/configs.py
+++ src/configs/configs.py
@@ -29,7 +29,7 @@ class Settings(BaseSettings):
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://ai-questionary-front-iyek.vercel.app",
-        "https://aiquestionaryfront-production.up.railway.app/",
+        "https://aiquestionaryfront-production.up.railway.app",
     ]
 
     @field_validator("FRONTEND_ORIGINS", mode="before")
